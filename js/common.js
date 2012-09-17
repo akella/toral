5382b71e2c5e60004750ff01a6276c3d3e5922a0
mirantel <magi.mirantel@gmail.com>
2012-09-17 19:28:30 +0300

    filter.html - 2 tabs + product.html

--- js/common.js
+++ js/common.js
@@ -1,24 +1,30 @@
-$(function() {
+$(document).ready(function(){
 	
-// slider
-	$( "#slider-range" ).slider({
-		orientation: "vertical",
-		range: true,
-		values: [ 1000, 3000 ],
-		min:0,
-		max:4000,
-		slide: function( event, ui ) {
-			$("#scale-diapason__to").val(ui.values[ 1 ] + " р.");
-			$("#scale-diapason__from").val(ui.values[ 0 ] + " р.");
-			$(".ui-slider-handle").eq(0).children().html(ui.values[ 0 ] + " р.");
-			$(".ui-slider-handle").eq(1).children().html(ui.values[ 1 ] + " р.");
-		}
-	});
-	$("#scale-diapason__to").val($( "#slider-range" ).slider( "values", 1 ) + " р.");
-	$("#scale-diapason__from").val($( "#slider-range" ).slider( "values", 0 ) + " р.");
-	$(".ui-slider-handle").eq(0).html("<span>"+ $( "#slider-range" ).slider( "values", 0 ) +" р.</span>");
-	$(".ui-slider-handle").eq(1).html("<span>"+ $( "#slider-range" ).slider( "values", 1 ) +" р.</span>");
-	
+	// function element exists
+	jQuery.fn.exists = function() {
+		return $(this).length;
+	}
+
+	// price slider
+	if ($("#slider-range").exists()){
+		$("#slider-range").slider({
+			orientation: "vertical",
+			range: true,
+			values: [ 1000, 3000 ],
+			min:0,
+			max:4000,
+			slide: function( event, ui ) {
+				$("#scale-diapason__to").val(ui.values[ 1 ] + " р.");
+				$("#scale-diapason__from").val(ui.values[ 0 ] + " р.");
+				$(".ui-slider-handle").eq(0).children().html(ui.values[ 0 ] + " р.");
+				$(".ui-slider-handle").eq(1).children().html(ui.values[ 1 ] + " р.");
+			}
+		});
+		$("#scale-diapason__to").val($( "#slider-range" ).slider( "values", 1 ) + " р.");
+		$("#scale-diapason__from").val($( "#slider-range" ).slider( "values", 0 ) + " р.");
+		$(".ui-slider-handle").eq(0).html("<span>"+ $( "#slider-range" ).slider( "values", 0 ) +" р.</span>");
+		$(".ui-slider-handle").eq(1).html("<span>"+ $( "#slider-range" ).slider( "values", 1 ) +" р.</span>");
+	};
 
 	// tabs
 	$('ul.tabs__nav').delegate('li:not(.active)', 'click', function() {
@@ -27,4 +33,44 @@ $(function() {
 		return false;
 	});
 	
+
+	// change quantity
+	$('.qty__more').click(function(){
+		var count = parseInt($(this).prev('input').val());
+		if (count < 1) {$(this).prev('input').val(1);} 
+		else {$(this).prev('input').val(count+1);}
+		return false;
+	});
+	
+	$('.qty__less').click(function(){
+		var count = parseInt($(this).next('input').val());
+		if (count <= 1) {$(this).next('input').val(1);} 
+		else {$(this).next('input').val(count-1);}
+		return false;
+	});
+
+	//zoom
+	if ($(".zoom").exists()){
+
+		$(window).load(function() {
+			var pic = $('.zoom img');
+			pic.removeAttr("width"); 
+			pic.removeAttr("height");
+			$('.zoom').width(pic.width());
+			$('.zoom').height(pic.height()).css('marginTop', function(index) {
+				return Math.ceil((330 - pic.height())/2);
+			});
+		});
+
+		var options = {
+			alwaysOn:true,
+			zoomWidth: 293,
+			zoomHeight: 293,
+			title:'false',
+		}; 
+		$('.zoom').jqzoom(options);
+	};
+
 });
+
+
